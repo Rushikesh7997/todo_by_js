8e60fae7c17b37028e4c9dcb7b3cc7dd9e64b045
Rushikesh <pardhikar26@gmail.com>
2025-02-04 22:25:44 +0530

    update women and men page

---
+++ E-commerce/mensData.js
@@ -0,0 +1,139 @@
+var mensData = [
+    {
+      product_name: "Wireless Headphones",
+      img_url:
+        "https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/1.png",
+      price: 59.99,
+      strikeoff_price: 99.99,
+    },
+    {
+      product_name: "Smartphone",
+      img_url:
+        "https://cdn.dummyjson.com/products/images/beauty/Eyeshadow%20Palette%20with%20Mirror/1.png",
+      price: 699.99,
+      strikeoff_price: 799.99,
+    },
+    {
+      product_name: "Gaming Mouse",
+      img_url:
+        "https://cdn.dummyjson.com/products/images/beauty/Powder%20Canister/1.png",
+      price: 29.99,
+      strikeoff_price: 49.99,
+    },
+    {
+      product_name: "Mechanical Keyboard",
+      img_url:
+        "https://cdn.dummyjson.com/products/images/beauty/Red%20Lipstick/1.png",
+      price: 89.99,
+      strikeoff_price: 129.99,
+    },
+    {
+      product_name: "4K Monitor",
+      img_url:
+        "https://cdn.dummyjson.com/products/images/beauty/Red%20Nail%20Polish/1.png",
+      price: 299.99,
+      strikeoff_price: 399.99,
+    },
+    {
+      product_name: "Smartwatch",
+      img_url:
+        "https://cdn.dummyjson.com/products/images/fragrances/Calvin%20Klein%20CK%20One/1.png",
+      price: 199.99,
+      strikeoff_price: 249.99,
+    },
+    {
+      product_name: "Bluetooth Speaker",
+      img_url:
+        "https://cdn.dummyjson.com/products/images/fragrances/Calvin%20Klein%20CK%20One/2.png",
+      price: 49.99,
+      strikeoff_price: 79.99,
+    },
+    {
+      product_name: "Laptop Backpack",
+      img_url:
+        "https://cdn.dummyjson.com/products/images/fragrances/Calvin%20Klein%20CK%20One/3.png",
+      price: 39.99,
+      strikeoff_price: 59.99,
+    },
+    {
+      product_name: "Portable SSD 1TB",
+      img_url:
+        "https://cdn.dummyjson.com/products/images/fragrances/Chanel%20Coco%20Noir%20Eau%20De/1.png",
+      price: 129.99,
+      strikeoff_price: 179.99,
+    },
+    {
+      product_name: "Ergonomic Office Chair",
+      img_url:
+        "https://cdn.dummyjson.com/products/images/fragrances/Chanel%20Coco%20Noir%20Eau%20De/2.png",
+      price: 199.99,
+      strikeoff_price: 299.99,
+    },
+  ];
+
+localStorage.setItem("mensData",JSON.stringify(mensData));
+
+var womenData = [
+    {
+      "product_name": "Floral Maxi Dress",
+      "img_url": "https://cdn.dummyjson.com/products/images/fragrances/Calvin%20Klein%20CK%20One/3.png",
+      "price": 49.99,
+      "strikeoff_price": 79.99
+    },
+    {
+      "product_name": "Leather Handbag",
+      "img_url": "https://cdn.dummyjson.com/products/images/fragrances/Calvin%20Klein%20CK%20One/3.png",
+      "price": 89.99,
+      "strikeoff_price": 129.99
+    },
+    {
+      "product_name": "Women’s Running Shoes",
+      "img_url": "https://cdn.dummyjson.com/products/images/fragrances/Calvin%20Klein%20CK%20One/3.png",
+      "price": 69.99,
+      "strikeoff_price": 99.99
+    },
+    {
+      "product_name": "Gold Plated Necklace",
+      "img_url": "https://cdn.dummyjson.com/products/images/fragrances/Calvin%20Klein%20CK%20One/3.png",
+      "price": 29.99,
+      "strikeoff_price": 59.99
+    },
+    {
+      "product_name": "Denim Jacket",
+      "img_url": "https://cdn.dummyjson.com/products/images/fragrances/Calvin%20Klein%20CK%20One/3.png",
+      "price": 59.99,
+      "strikeoff_price": 89.99
+    },
+    {
+      "product_name": "Silk Scarf",
+      "img_url": "https://cdn.dummyjson.com/products/images/fragrances/Calvin%20Klein%20CK%20One/3.png",
+      "price": 19.99,
+      "strikeoff_price": 39.99
+    },
+    {
+      "product_name": "Luxury Perfume",
+      "img_url": "https://cdn.dummyjson.com/products/images/fragrances/Calvin%20Klein%20CK%20One/3.png",
+      "price": 99.99,
+      "strikeoff_price": 149.99
+    },
+    {
+      "product_name": "Casual Sneakers",
+      "img_url": "https://cdn.dummyjson.com/products/images/fragrances/Calvin%20Klein%20CK%20One/3.png",
+      "price": 54.99,
+      "strikeoff_price": 79.99
+    },
+    {
+      "product_name": "Summer Hat",
+      "img_url": "https://cdn.dummyjson.com/products/images/fragrances/Calvin%20Klein%20CK%20One/3.png",
+      "price": 24.99,
+      "strikeoff_price": 39.99
+    },
+    {
+      "product_name": "Evening Clutch Bag",
+      "img_url": "https://cdn.dummyjson.com/products/images/fragrances/Calvin%20Klein%20CK%20One/3.png",
+      "price": 44.99,
+      "strikeoff_price": 69.99
+    }
+  ]
+  
+localStorage.setItem("womenData",JSON.stringify(womenData))
