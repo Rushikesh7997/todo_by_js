1d89d6d55e9ae502da9cb659de436a215f024906
Rushikesh <pardhikar26@gmail.com>
2025-02-17 23:42:25 +0530

    add toast notification

---
+++ Toast Notification/script.js
@@ -0,0 +1,20 @@
+let toastBox = document.getElementById("toast-box");
+let successMsg = "<i class='fa-solid fa-circle-check'></i> Successful";
+let errorMsg = "<i class='fa-solid fa-circle-xmark'></i> Something went wrong";
+let invalidMsg = "<i class='fa-solid fa-circle-exclamation'></i> Invalid Input";
+
+function showToast(msg){
+    let toast = document.createElement('div');
+    toast.classList.add('toast');
+    toast.innerHTML = msg;
+    toastBox.appendChild(toast);
+
+    if(msg.includes('wrong')){
+        toast.classList.add('error')
+    }
+    if(msg.includes('Invalid')){
+        toast.classList.add('invalid')
+    }
+}
+
+
