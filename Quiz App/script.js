2843677191f9e3c84d89547bb6973edb89397d27
Rushikesh <pardhikar26@gmail.com>
2025-02-19 23:35:35 +0530

    quiz app

---
+++ Quiz App/script.js
@@ -0,0 +1,67 @@
+const question = [
+    {
+        question: "which is largest animal in the world ? ",
+        answer:[
+            {text: "Shark", correct: false},
+            {text: "Blue Whale", correct: false},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+        ]
+    },
+    {
+        question: "which is largest animal in the world ? ",
+        answer:[
+            {text: "Shark", correct: false},
+            {text: "Blue Whale", correct: false},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+        ]
+    },
+    {
+        question: "which is largest animal in the world ? ",
+        answer:[
+            {text: "Shark", correct: false},
+            {text: "Blue Whale", correct: false},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+        ]
+    },
+    {
+        question: "which is largest animal in the world ? ",
+        answer:[
+            {text: "Shark", correct: false},
+            {text: "Blue Whale", correct: false},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+        ]
+    },
+    {
+        question: "which is largest animal in the world ? ",
+        answer:[
+            {text: "Shark", correct: false},
+            {text: "Blue Whale", correct: false},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+        ]
+    }
+]
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-button");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    let currentQuestion = question[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    
+}
