bc86b7ccf1a4d350abd5302a127032b95b1c2d02
Rushikesh <pardhikar26@gmail.com>
2025-02-21 18:19:52 +0530

    weather app update

--- Weather App/script.js
+++ Weather App/script.js
@@ -1,4 +1,4 @@
-const apiKey = "74b21dde87245e0f2283415315cf54af";
+const apiKey = "";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchBox = document.querySelector(".search input");
